6936ce81be475d7edadb10780603c866145cc507
Peter Zeng <pzeng.pzeng@gmail.com>
2019-01-10 16:51:57 -0800

    Add main app.js file to grab canvas element and allow game to start with context passed in as an arg

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
 
-  let game = new Game(ctx, canvas);
+  let game = new Game(ctx);
   game.animate();
 });
