db7123d43b88f45714ca0465f4935a7f31cfc5f8
Peter Zeng <pzeng.pzeng@gmail.com>
2019-01-13 20:54:11 -0800

    Update app.js and package.json to push to push to heroku

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("frontend/build"));
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.use(express.static("dist"));
 
