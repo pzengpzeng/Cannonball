33bb7568a3d7ec3022330dca6801b852fe4dc76a
Peter Zeng <pzeng.pzeng@gmail.com>
2019-01-13 19:29:59 -0800

    Remove test code related to axios calls in app.js. Moved to game.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,8 @@
-const axios = require("axios");
-
 import Game from "./game";
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-
-  // const leaderboard = document.getElementById("leaderboard");
-
-  // const fetchScores = () => {
-  //   return axios
-  //     .get("api/scores")
-  //     .then(response => {
-  //       console.log(response);
-  //     })
-  //     .catch(function(error) {
-  //       console.log(error);
-  //     });
-  // };
-
-  // leaderboard.innerHTML = fetchScores();
 
   let game = new Game(ctx);
   game.animate();
