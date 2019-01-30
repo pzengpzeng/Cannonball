f2425610a0196904d6ef5809ebe79336c2b8c923
Peter Zeng <pzeng.pzeng@gmail.com>
2019-01-30 14:33:54 -0800

    Deconstruct x and y positions of cannon inside constructor

--- src/cannon.js
+++ src/cannon.js
@@ -7,9 +7,8 @@ cannonFullRightImage.src = "../assets/images/monkey-loaded-right.png";
 
 class Cannon {
   constructor(xPos, verticalD, horizontalD) {
-    let yPos = Math.random() * 450 + 50;
-
-    this.position = [xPos, yPos];
+    this.xPos = xPos;
+    this.yPos = Math.random() * 450 + 50;
     this.emptyLeftImage = cannonEmptyLeftImage;
     this.emptyRightImage = cannonEmptyRightImage;
     this.fullRightImage = cannonFullRightImage;
@@ -21,28 +20,28 @@ class Cannon {
 
   drawStationary(ctx) {
     let image = this.selectImage();
-    ctx.drawImage(image, this.position[0], this.position[1], 90, 90);
+    ctx.drawImage(image, this.xPos, this.yPos, 90, 90);
   }
 
   drawRotated(ctx, degrees) {
     let image = this.selectImage();
 
     ctx.save();
-    ctx.translate(this.position[0], this.position[1]);
+    ctx.translate(this.xPos, this.yPos);
     ctx.rotate((degrees * Math.PI) / 180);
     ctx.drawImage(image, -image.width / 2, -image.height / 2, 90, 90);
     ctx.restore();
   }
 
   moveY() {
-    this.position[1] += this.verticalD;
-    if (this.position[1] <= 0 || this.position[1] + 90 >= 600) {
+    this.yPos += this.verticalD;
+    if (this.yPos <= 0 || this.yPos + 90 >= 600) {
       this.verticalD = -this.verticalD;
     }
   }
 
   moveX(speed) {
-    this.position[0] -= speed;
+    this.xPos -= speed;
   }
 
   selectImage() {
