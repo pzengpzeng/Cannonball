14f37cc2b3bf5e3e99e578beb9908129754755f0
Peter Zeng <pzeng.pzeng@gmail.com>
2019-01-30 14:34:21 -0800

    Deconstruct x and y positions of cannon inside constructor

--- src/monkey.js
+++ src/monkey.js
@@ -3,21 +3,22 @@ monkeyBallImage.src = "../assets/images/monkey-ball.png";
 
 class Monkey {
   constructor(xPos, yPos) {
-    this.position = [xPos, yPos];
+    this.xPos = xPos;
+    this.yPos = yPos;
     this.ballImage = monkeyBallImage;
     this.degrees = 0;
   }
 
   drawRotated(ctx, degrees) {
     ctx.save();
-    ctx.translate(this.position[0], this.position[1]);
+    ctx.translate(this.xPos, this.yPos);
     ctx.rotate((degrees * Math.PI) / 180);
     ctx.drawImage(this.ballImage, -60 / 2, -60 / 2, 60, 60);
     ctx.restore();
   }
 
   move() {
-    this.position[0] += 4;
+    this.xPos += 8;
   }
 }
 
