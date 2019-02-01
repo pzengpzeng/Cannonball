fe5d3199be4f2b62d208b36381b05b6a0e9ee0c3
Peter Zeng <pzeng.pzeng@gmail.com>
2019-02-01 15:19:55 -0800

    Create single source of constants used throughout the game

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+// Images
+export const backgroundImg = new Image();
+backgroundImg.src = "../assets/images/background.jpeg";
+
+export const cannonEmptyLeftImg = new Image();
+cannonEmptyLeftImg.src = "../assets/images/cannon-empty-left.png";
+
+export const cannonEmptyRightImg = new Image();
+cannonEmptyRightImg.src = "../assets/images/cannon-empty-right.png";
+
+export const monkeyBallImg = new Image();
+monkeyBallImg.src = "../assets/images/monkey-ball.png";
+
+export const cannonFullRightImg = new Image();
+cannonFullRightImg.src = "../assets/images/monkey-loaded-right.png";
+
+// Colors
+export const black = "black";
+export const white = "white";
+export const yellow = "#F5C028";
+
+// Blink Intervals
+
+export const halfInterval = 70;
+export const fullInterval = 140;
