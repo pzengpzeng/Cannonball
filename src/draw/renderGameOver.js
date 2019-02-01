bf75ce0a2e2471ee0d33cd5ec140861a2a373fcf
Peter Zeng <pzeng.pzeng@gmail.com>
2019-02-01 15:59:56 -0800

    Refactor renderGameOver method to use highScoreText and recentScoreText functions which create objects that can then be used with renderText function

--- src/draw/renderGameOver.js
+++ src/draw/renderGameOver.js
@@ -1,26 +1,19 @@
 import {
   gameOverText,
+  highScoreText,
+  recentScoreText,
   continuePlaying,
   continuePlayingAlt
 } from "./textObjects";
 
 import { renderText, renderBlinkingText } from "./renderText";
 
-export function renderGameOver(ctx, highScore, score) {
+export function renderGameOver(ctx, highScore, recentScore) {
   ctx.textAlign = "center";
   ctx.lineWidth = 4;
 
   renderText(ctx, ...Object.values(gameOverText));
-
-  ctx.font = "50px 'Teko'";
-  ctx.strokeStyle = "white";
-  ctx.fillStyle = "black";
-
-  ctx.strokeText(`Your best score : ${highScore}`, 500, 300);
-  ctx.fillText(`Your best score : ${highScore}`, 500, 300);
-
-  ctx.strokeText(`Recent score : ${score}`, 500, 350);
-  ctx.fillText(`Recent score : ${score}`, 500, 350);
-
+  renderText(ctx, ...Object.values(highScoreText(highScore)));
+  renderText(ctx, ...Object.values(recentScoreText(recentScore)));
   renderBlinkingText(ctx, continuePlaying, continuePlayingAlt);
 }
