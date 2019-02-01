8d0b0c5aac501582bf24a2bd3e57bbb63f0617d1
Peter Zeng <pzeng.pzeng@gmail.com>
2019-02-01 15:21:45 -0800

    Separate drawBackground function out of draw.js and move into own file

---
+++ src/draw/drawBackground.js
@@ -0,0 +1,16 @@
+import { backgroundImg } from "../constants";
+
+export function drawBackground(ctx, score, username) {
+  ctx.drawImage(backgroundImg, 0, 0, 1000, 600);
+
+  ctx.textAlign = "right";
+  ctx.font = "30px 'Teko'";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 4;
+
+  ctx.strokeText(`Score : ${score}`, 990, 60);
+  ctx.strokeText(`Welcome , ${username}!`, 990, 30);
+  ctx.fillStyle = "#F5C028";
+  ctx.fillText(`Score : ${score}`, 990, 60);
+  ctx.fillText(`Welcome , ${username}!`, 990, 30);
+}
